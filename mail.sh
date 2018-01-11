41ce79b0248d8a3a4ae464b6c884cbbfb227f072
root <root@pete.gonzalonazareno.org>
2018-01-11 08:06:43 +0000

    correo y borrado

---
+++ mail.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Mañana se borrarrán todas las copias hechas hasta ahora y se hara una copia completa que se almacenará permanentemente" | sendmail miguelchico14@gmail.com
--- restaura.sh
+++ restaura.sh
@@ -11,24 +11,24 @@ read full
 echo "Introduzca fecha de la incremental(dd-mm-yyyy): "
 read inc
 
-hostname=`ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip hostname -s`
+hostname=`ssh root@$ip hostname -s`
 scp /backups/$hostname/Full_Backup_$full.tar.gz root@$ip:/tmp/
 scp /backups/$hostname/inc_Backup_$inc.tar.gz root@$ip:/tmp/
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip "tar -xzpf /tmp/Full_Backup_$full.tar.gz -C /"
-cont=`ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip ls /tmp/backups/ | wc -l`
+ssh root@$ip "tar -xzpf /tmp/Full_Backup_$full.tar.gz -C /"
+cont=`ssh root@$ip ls /tmp/backups/ | wc -l`
 while [ $cont -ne 0 ];
 do
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip "tar -xzpf /tmp/backups/`ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip ls /tmp/backups/ | head -$cont | tail -1` -C /"
+	ssh root@$ip "tar -xzpf /tmp/backups/`ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip ls /tmp/backups/ | head -$cont | tail -1` -C /"
 	cont=$(($cont-1))
 done
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip rm -r /tmp/backups
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip "tar -xzpf /tmp/inc_Backup_$inc.tar.gz -C /"
-cont=`ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip ls /tmp/backups/ | wc -l`
+ssh root@$ip rm -r /tmp/backups
+ssh root@$ip "tar -xzpf /tmp/inc_Backup_$inc.tar.gz -C /"
+cont=`ssh root@$ip ls /tmp/backups/ | wc -l`
 while [ $cont -ne 0 ];
 do
-        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip "tar -xzpf /tmp/backups/`ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip ls /tmp/backups/ | head -$cont | tail -1` -C /"
+        ssh root@$ip "tar -xzpf /tmp/backups/`ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip ls /tmp/backups/ | head -$cont | tail -1` -C /"
         cont=$(($cont-1))
 done
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip rm -r /tmp/backups
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip rm /tmp/Full_Backup_$full.tar.gz
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip rm /tmp/inc_Backup_$inc.tar.gz
+ssh root@$ip rm -r /tmp/backups
+ssh root@$ip rm /tmp/Full_Backup_$full.tar.gz
+ssh root@$ip rm /tmp/inc_Backup_$inc.tar.gz
